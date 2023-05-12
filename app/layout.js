964cc448b5b9caeebd6e5e1bec720855711430f8
Lindrit <lindritsulaj2.0@gmail.com>
2023-05-12 19:16:35 +0200

    Setup

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Navbar } from '@/components'
+import { Manrope } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const manrope = Manrope({ subsets: ['latin'], weight: [ '200', '300', '400', '500', '600', '700', '800'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={manrope.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
